38201799467dc1c996df33a9352cbc926663bcb3
maucepinho <mauriciocepinho@gmail.com>
2022-10-13 17:25:31 -0300

    Ta indo

--- Calculadora/js.js
+++ Calculadora/js.js
@@ -1,46 +1,63 @@
 $( function(){
     $(".button").click(function(){
-        $("#result").html($(this).val());
-        // const valor = $(".button").val();
-        // console.log("Botão apertado");
-        // console.log(valor);
-        // console.log($(".button").val())
+        $("#result").val($("#result").val() + $(this).val());        
+    });
+
+    $("#clean").click(function(){
+        $("#result").val("");
     });
 });
 
 
-// $("#getValBtnID").click(function () {
-//     var inputString = $("#userInputID").val();
-//     alert(inputString);
-// });
-// $("#setBtnID").click(function () {
-//     $("input:text").val("Hello World!");
-
-// Teste da internet https://www.youtube.com/watch?v=7GviAKxBxsY (https://acervolima.com/projetar-e-implementar-calculadora-usando-jquery/)
-        // $(document).ready(function() {
-        //     //Adding to the expression
-        //     $(".design").click(function() {
-        //         if (!$(this).hasClass("not")) {
-        //             if ($("#expression").val() == 0)
-        //                 $("#expression").val($(this).text());
-        //             else
-        //                 $("#expression").val($(
-        //                   "#expression").val() + $(this).text());
-        //         }
-        //     });
+{/* <script>
+        $(document).ready(function() {
+            //Adding to the expression
+            $(".design").click(function() {
+                if (!$(this).hasClass("not")) {
+                    if ($("#expression").val() == 0)
+                        $("#expression").val($(this).text());
+                    else
+                        $("#expression").val($("#expression").val() + $(this).text());
+                }
+            });
   
-        //     //Backspace
-        //     $('#backspace').click(function() {
-        //         var value = $("#expression").val();
-        //         if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
-        //             $("#expression").val(value.slice(0, value.length - 1));
-        //         if (value.length == 1)
-        //             $("#expression").val("0");
-        //     });
-        // });
+            //Backspace
+            $('#backspace').click(function() {
+                var value = $("#expression").val();
+                if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
+                    $("#expression").val(value.slice(0, value.length - 1));
+                if (value.length == 1)
+                    $("#expression").val("0");
+            });
+        });
   
-        // // All Clear
-        // $("#allClear").click(function() {
-        //     $("#expression").val("0");
-        // });
-    
+        // All Clear
+        $("#allClear").click(function() {
+            $("#expression").val("0");
+            $("#result").val("0");
+        });
+  
+        //Evalution
+        $("#equals").click(function() {
+            var result;
+            //Check for syntax error
+            try {
+                result = (eval(($("#expression").val())));
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    alert("Error! Resetting values.");
+                    $("#expression").val("0");
+                    $("#result").val("0");
+                }
+                if (e instanceof TypeError) {
+                    alert("Error! Resetting values.");
+                    $("#expression").val("0");
+                    $("#result").val("0");
+                }
+            }
+  
+            // Append if the result is correct
+            $("#result").val(result);
+            $("#expression").val("0");
+        });
+    </script> */}
