c9cf10cab098594755885a9fe508379fdb7a4ab1
maucepinho <mauriciocepinho@gmail.com>
2022-10-17 20:32:31 -0300

    Agora Funcionou!

--- Calculadora/js.js
+++ Calculadora/js.js
@@ -1,63 +1,28 @@
-$( function(){
-    $(".button").click(function(){
-        $("#result").val($("#result").val() + $(this).val());        
-    });
-
-    $("#clean").click(function(){
-        $("#result").val("");
-    });
+$(".button").click(function() {
+    if (!$(this).hasClass("not")) {
+        if ($("#expression").val() == 0)
+            $("#expression").val($(this).val());
+        else
+            $("#expression").val($("#expression").val() + $(this).val());
+    }
 });
 
+$("#backspace").click(function() {
+    var value = $("#expression").val();
+    if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
+        $("#expression").val(value.slice(0, value.length - 1));
+    if (value.length == 1)
+        $("#expression").val("0");
+});
 
-{/* <script>
-        $(document).ready(function() {
-            //Adding to the expression
-            $(".design").click(function() {
-                if (!$(this).hasClass("not")) {
-                    if ($("#expression").val() == 0)
-                        $("#expression").val($(this).text());
-                    else
-                        $("#expression").val($("#expression").val() + $(this).text());
-                }
-            });
-  
-            //Backspace
-            $('#backspace').click(function() {
-                var value = $("#expression").val();
-                if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
-                    $("#expression").val(value.slice(0, value.length - 1));
-                if (value.length == 1)
-                    $("#expression").val("0");
-            });
-        });
-  
-        // All Clear
-        $("#allClear").click(function() {
-            $("#expression").val("0");
-            $("#result").val("0");
-        });
-  
-        //Evalution
-        $("#equals").click(function() {
-            var result;
-            //Check for syntax error
-            try {
-                result = (eval(($("#expression").val())));
-            } catch (e) {
-                if (e instanceof SyntaxError) {
-                    alert("Error! Resetting values.");
-                    $("#expression").val("0");
-                    $("#result").val("0");
-                }
-                if (e instanceof TypeError) {
-                    alert("Error! Resetting values.");
-                    $("#expression").val("0");
-                    $("#result").val("0");
-                }
-            }
-  
-            // Append if the result is correct
-            $("#result").val(result);
-            $("#expression").val("0");
-        });
-    </script> */}
+$("#clean").click(function() {
+    $("#expression").val("0");
+    $("#result").val("0");
+});
+
+$("#equal").click(function() {
+    var result;
+    result = (eval(($("#expression").val())));
+    $("#result").val(result);
+    $("#expression").val("0");
+});
